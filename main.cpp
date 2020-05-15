e88baaf9ca4c99170970d6851d9b297063838046
Nick <legokol3@gmail.com>
2020-05-15 12:53:00 +0300

    Correct random

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ using namespace std;
 
 int main() {
     double r[] = {6, 10}, m[] = {10, 20}, dt = 0.01;
+    int w = 1600, vmax = 400;
     unsigned int n = 500;
 
     vector<DrawableParticle> particles1;
@@ -27,20 +28,20 @@ int main() {
     particles2.reserve(n);
 
     for (unsigned int i = 0; i < n; i++) {
-        double x = (double) rand_uns(10, 790);
-        double y = (double) rand_uns(10, 790);
-        double vx = (double) rand_uns(0, 20) - 10;
-        double vy = (double) rand_uns(0, 20) - 10;
+        double x = (double) (rand_uns(0, w) % 800);
+        double y = (double) (rand_uns(0, w) % 1000);
+        double vx = (double) (rand_uns(0, w) % (2 * vmax) - vmax);
+        double vy = (double) (rand_uns(0, w) % (2 * vmax) - vmax);
         Vector2D loc(x, y);
         Vector2D v(vx, vy);
         particles1.push_back(DrawableParticle(m[0], r[0], loc, v));
         particles1[i].fill(255, 0, 0);
     }
     for (unsigned int i = 0; i < n; i++) {
-        double x = (double) rand_uns(10, 790);
-        double y = (double) rand_uns(10, 790);
-        double vx = (double) rand_uns(0, 20) - 10;
-        double vy = (double) rand_uns(0, 20) - 10;
+        double x = (double) (rand_uns(0, w) % 800 + 800);
+        double y = (double) (rand_uns(0, w) % 1000);
+        double vx = (double) (rand_uns(0, w) % (2 * vmax) - vmax);
+        double vy = (double) (rand_uns(0, w) % (2 * vmax) - vmax);
         Vector2D loc(x, y);
         Vector2D v(vx, vy);
         particles2.push_back(DrawableParticle(m[1], r[1], loc, v));
